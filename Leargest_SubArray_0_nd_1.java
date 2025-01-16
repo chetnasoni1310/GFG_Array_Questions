82f6b1fab236dd1f62771e7b5795d23ba51ff124
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-01-16 20:41:25 +0530

    updated

---
+++ Leargest_SubArray_0_nd_1.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class Leargest_SubArray_0_nd_1 {
+     public int maxLen(int[] arr) {
+        // Your code here
+        int n=arr.length;
+        if(n<=1)
+        {
+            return 0;
+        }
+        int maxlen=0;
+        
+        int cs=0; //cummulative sum
+        HashMap<Integer, Integer> set = new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {   
+            if(arr[i]==0)
+            {
+                arr[i]=-1;
+            }
+            cs+=arr[i];
+            if(cs==0)
+            {
+                maxlen=i+1;
+            }
+            if(set.containsKey(cs))
+            {
+                int sublen=i-set.get(cs);
+                maxlen=Math.max(maxlen,sublen);
+            }
+            else{
+            set.put(cs,i);
+            }
+            
+        }
+        return maxlen;
+    }
+}
